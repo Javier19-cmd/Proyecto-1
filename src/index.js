df568e28bf80a39069344bb65a2483166b24dda8
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-05-01 18:01:42 -0600

    cambiando cosas

--- src/index.js
+++ src/index.js
@@ -1,32 +1,21 @@
-/**
- * Instalaciones realizadas:
- * 
- * 1. Webpack: npm install webpack webpack-cli.
- * 2. React: npm install react react-dom.
- * 3. Babel: npm install babel-loader @babel/core @babel/preset-react.
- * 4. Webpack-dev-server: npm install webpack-dev-server
- * 5. npm install --save-dev style-loader
- * 6. npm install -D mini-css-extract-plugin
- * 7. npm install --save-dev css-loader
-*/
-
+//  Javier Valle
+//  Carnet 20159
 import React from 'react'
-import ReactDOM from 'react-dom'
 import * as ReactDOMClient from 'react-dom/client'
 import Header from './components/Header.jsx'
 
-const App = () => {
-    return (
-        <>
-          <Header title='Hola' />
-        </>
-    )
+function App() {
+  return (
+    <div>
+      <Header title="Hola" />
+    </div>
+  )
 }
 
 const root = ReactDOMClient.createRoot(
-    document.getElementById('root')
+  document.getElementById('root'),
 )
 
 root.render(
-    <App />
+  <App />,
 )
