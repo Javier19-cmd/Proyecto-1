c9c72f31d515f5fe28f1ef62597bf039399a3814
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-05-04 13:16:34 -0600

    arreglando cosas :v

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,40 @@
 //Configuración del webpack
 export default{
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-      filename: 'bundle.js',
+  mode: 'development',
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+  },
+  performance: {
+    hints: false,
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  //Creando configuración para el servidor.
+  devServer: {
+    static: {
+      directory: 'dist',
     },
-  
-    //Creando configuración para el servidor.
-    devServer: {
-      static: {
-        directory: 'dist',
-      },
-      compress: true,
-      port: 9000
+    compress: true,
+    port: 9000
+  },
+
+  //Configurando Babel, css y aceptando imágenes.
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      use: ['babel-loader'],
     },
-  
-    //Configurando Babel, css y aceptando imágenes.
-    module: {
-      rules: [{
-        test: /\.jsx?$/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-     },
-      {
-        type: 'asset',
-        test: /\.(png|svg|jpg|jpeg|gif)$/i
-      }
-      ]
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      type: 'asset',
+      test: /\.(png|svg|jpg|jpeg|gif)$/i
     }
+    ]
   }
+}
   
