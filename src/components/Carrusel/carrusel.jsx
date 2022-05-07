58a867318c0d8b871868a905d2e7225f71831f9f
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-05-07 11:21:37 -0600

    agregando cosas

--- src/components/Carrusel/carrusel.jsx
+++ src/components/Carrusel/carrusel.jsx
@@ -1,15 +1,60 @@
 import React from 'react'
 import styles from './carrusel.css'
+import Uno from './I1.jpg'
+import Dos from './I2.jpg'
+import Tres from './I3.jpg'
 
 export default function carrusel() {
   return (
-    <div className="itemCarrusel" style={styles}>
-      <div className="tarjetaCarrusel">
-        a
+    <div>
+      {/* Tarjetas de las imágenes */}
+      <div id="conteItemsCarrusel" style={styles}>
+        <div className="itemCarrusel" id="itemCarrusel-1">
+          <div className="tarjetaCarrusel" id="tarjetaCarrusel-1">
+            <img className="Img1" src={Uno} alt="Imagen 1" />
+          </div>
+          <div className="flechasCarrusel">
+            <a href="#itemCarrusel-3">
+              <i>I</i>
+            </a>
+            <a href="#itemCarrusel-2">
+              <i>D</i>
+            </a>
+          </div>
+        </div>
+        <div className="itemCarrusel" id="itemCarrusel-2">
+          <div className="tarjetaCarrusel" id="tarjetaCarrusel-2">
+            <img className="Img1" src={Dos} alt="Imagen 2" />
+          </div>
+          <div className="flechasCarrusel">
+            <a href="#itemCarrusel-1">
+              <i>I</i>
+            </a>
+            <a href="#itemCarrusel-3">
+              <i>D</i>
+            </a>
+          </div>
+        </div>
+
+        <div className="itemCarrusel" id="itemCarrusel-3">
+          <div className="tarjetaCarrusel" id="tarjetaCarrusel-3">
+            <img className="Img1" src={Tres} alt="Imagen 3" />
+          </div>
+          <div className="flechasCarrusel">
+            <a href="#itemCarrusel-2">
+              <i>I</i>
+            </a>
+            <a href="#itemCarrusel-1">
+              <i>D</i>
+            </a>
+          </div>
+        </div>
       </div>
-      <div className="flechasCarrusel">
-        <i>I</i>
-        <i>D</i>
+
+      <div id="contePuntos">
+        <a href="#itemCarrusel-1">-</a>
+        <a href="#itemCarrusel-2">-</a>
+        <a href="#itemCarrusel-3">-</a>
       </div>
     </div>
   )
