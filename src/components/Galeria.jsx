94bcd9374f3b0c8463d54d9c9ba94c92f4428b28
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-05-11 13:46:24 -0600

    creando la mini navegación

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -5,12 +5,30 @@ function Galeria() {
   return (
     <div className="Wrapper" style={styles}>
       <header>Pure CSS Tabs</header>
+
+      <input type="radio" name="slider" id="productos" checked />
+      <input type="radio" name="slider" id="smartphones" />
+      <input type="radio" name="slider" id="tablets" />
+      <input type="radio" name="slider" id="TV" />
+      <input type="radio" name="slider" id="Elec" />
+      
       <nav>
-        <label htmlFor="" className="home"> <i className="Destacados">Productos Destacados</i> </label>
-        <label htmlFor="" className="home"> <i className="Smartphones">Smartphones</i> </label>
-        <label htmlFor="" className="home"> <i className="Tablets">Tablets & Wearebles</i> </label>
-        <label htmlFor="" className="home"> <i className="Av">Tv & Av</i> </label>
-        <label htmlFor="" className="home"> <i className="Electrodos">Electrodomésticos</i> </label>
+        <label htmlFor="productos" name="productos">
+          <i className="Destacados">Productos Destacados</i>
+        </label>
+        <label htmlFor="smartphones" name="smartphones">
+          <i className="Smartphones">Smartphones</i>
+        </label>
+        <label htmlFor="tablets" name="tablets">
+          <i className="Tablets">Tablets & Wearebles</i>
+        </label>
+        <label htmlFor="TV" name="TV">
+          <i className="Av">Tv & Av</i>
+        </label>
+        <label htmlFor="Elec" name="Elec">
+          <i className="Electrodos">Electrodomésticos</i>
+        </label>
+        <div className="slider"></div>
       </nav>
     </div>
   )
