d073b0c1b8140b632e59b0b5921766d9e1d01a44
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-05-15 19:06:53 -0600

    ;v;

--- src/components/Pie.jsx
+++ src/components/Pie.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import styles from './Pie.css'
+import facebook from './facebook.png'
+import instagram from './instagram.png'
+import linkedin from './linkedin.png'
+import youtube from './youtube.png'
+import twitter from './twitter.png'
 
 function Pie() {
   return (
@@ -19,6 +24,7 @@ function Pie() {
           <a href="https://www.samsung.com/latin/info/legal/?_ga=2.195229608.503100336.1605699783-1958430373.1605699783">Legal</a>
         </div>
 
+        {/* Secciones de la página */}
         <div className="Secciones">
           <p className="Sec">Otras secciones</p>
           <a className="Conocenos" href="https://www.samsung.com/latin/about-us/company-info/">Conócenos</a>
@@ -26,6 +32,7 @@ function Pie() {
           <a className="Ventas" href="https://shop.samsung.com/latin/gt/b2b?___from_store=pa">Ventas a Empresas - B2B</a>
         </div>
 
+        {/* Apartado del soporte técnico */}
         <div className="Soporte">
           <p className="Necesitas">¿Necesitas soporte?</p>
           <a className="Telefonico" href="https://shop.samsung.com/latin/gt/faqs">Soporte telefónico</a>
@@ -33,16 +40,29 @@ function Pie() {
           <a className="Preguntas" href="https://shop.samsung.com/latin/gt/faqs">Preguntas frecuentes</a>
         </div>
 
+        {/* Sitios oficiales de cada país */}
         <div className="Sitios">
           <p className="Oficiales">Sitios Oficiales</p>
           <a className="Costa-Rica" href="https://www.samsung.com/latin/microsite/costa-rica/">Samsung Costa Rica</a>
-          <a className="Ecuador" herf="https://www.samsung.com/latin/microsite/ecuador/">Samsung Ecuador</a>
-          <a className="El-Salvador" herf="https://www.samsung.com/latin/microsite/el-salvador/">Samsung El Salvador</a>
+          <a className="Ecuador" href="https://www.samsung.com/latin/microsite/ecuador/">Samsung Ecuador</a>
+          <a className="El-Salvador" href="https://www.samsung.com/latin/microsite/el-salvador/">Samsung El Salvador</a>
           <a className="Guatemala" href="https://www.samsung.com/latin/microsite/guatemala/">Samsung Guatemala</a>
           <a className="Honduras" href="https://www.samsung.com/latin/microsite/honduras/">Samsung Honduras</a>
           <a className="Nicaragua" href="https://www.samsung.com/latin/microsite/nicaragua/">Samsung Nicaragua</a>
-          <a className="Panama" herf="https://www.samsung.com/latin/microsite/panama/">Samsung Panamá</a>
-          <a className="Republica-Dominicana">República Dominicana</a>
+          <a className="Panama" href="https://www.samsung.com/latin/microsite/panama/">Samsung Panamá</a>
+          <a className="Republica-Dominicana" href="https://www.samsung.com/latin/microsite/republica-dominicana/">República Dominicana</a>
+          <a className="Venzuela" href="https://www.samsung.com/latin/microsite/venezuela/">Venezuela</a>
+        </div>
+
+        {/* Íconos de las redes sociales */}
+
+        <div className="Redes">
+          <p className="Redes-Sociales">Redes Sociales</p>
+          <img className="Face" src={facebook} alt="Facebook" />
+          <img className="Twi" src={twitter} alt="Twitter" />
+          <img className="Insta" src={instagram} alt="Instagram" />
+          <img className="You" src={youtube} alt="YouTube" />
+          <img className="Linke" src={linkedin} alt="Linkedlin" />
         </div>
       </footer>
     </div>
