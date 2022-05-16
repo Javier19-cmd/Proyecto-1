3df908610a4b12ed06810bc16568a21598a937a8
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-05-15 18:38:19 -0600

    arreglando identaciones

--- src/components/CuadroNegro.jsx
+++ src/components/CuadroNegro.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import styles from './CuadroNegro.css'
 import Cuadro from './Imagen23.png'
+
 function CuadroNegro() {
-    return (
-      <div className="Gene" style={styles}>
-        <img className="Cuadrito" src={Cuadro} alt="Cuadro"/>
-      </div>
-    )
+  return (
+    <div className="Gene" style={styles}>
+      <img className="Cuadrito" src={Cuadro} alt="Cuadro" />
+    </div>
+  )
 }
 
 export default CuadroNegro
